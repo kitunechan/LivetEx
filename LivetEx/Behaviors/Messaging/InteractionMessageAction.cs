06061f94b0988019d4566526ad8e09e806564f35
kitunechan <22765277+kitunechan@users.noreply.github.com>
2017-02-07 17:48:59 +0900

    InvokeActionOnlyWhenWindowIsActiveのデフォルト値をFalseへ変更

--- LivetEx/Behaviors/Messaging/InteractionMessageAction.cs
+++ LivetEx/Behaviors/Messaging/InteractionMessageAction.cs
@@ -4,78 +4,69 @@ using System.Windows.Interactivity;
 using LivetEx.Messaging;
 using System.ComponentModel;
 
-namespace LivetEx.Behaviors.Messaging
-{
-    /// <summary>
-    /// ViewModelからの相互作用メッセージに対応するアクションの基底抽象クラスです<br/>
-    /// 独自のアクションを定義する場合はこのクラスを継承してください。
-    /// </summary>
-    /// <typeparam name="T">このアクションがアタッチ可能な型を示します。</typeparam>
-    [System.Windows.Markup.ContentProperty("DirectInteractionMessage")]
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1000:DoNotDeclareStaticMembersOnGenericTypes")]
-    public abstract class InteractionMessageAction<T> : TriggerAction<T> where T : DependencyObject
-    {
-        protected override sealed void Invoke(object parameter)
-        {
-            if ((bool)(DesignerProperties.IsInDesignModeProperty.GetMetadata(typeof(DependencyObject)).DefaultValue)) return;
+namespace LivetEx.Behaviors.Messaging {
+	/// <summary>
+	/// ViewModelからの相互作用メッセージに対応するアクションの基底抽象クラスです<br/>
+	/// 独自のアクションを定義する場合はこのクラスを継承してください。
+	/// </summary>
+	/// <typeparam name="T">このアクションがアタッチ可能な型を示します。</typeparam>
+	[System.Windows.Markup.ContentProperty( "DirectInteractionMessage" )]
+	[System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1000:DoNotDeclareStaticMembersOnGenericTypes" )]
+	public abstract class InteractionMessageAction<T>: TriggerAction<T> where T: DependencyObject {
+		protected override sealed void Invoke( object parameter ) {
+			if( (bool)( DesignerProperties.IsInDesignModeProperty.GetMetadata( typeof( DependencyObject ) ).DefaultValue ) ) return;
 
-            var message = parameter as InteractionMessage;
-            
-            if(DirectInteractionMessage != null)
-            {
-                message = DirectInteractionMessage.Message;
-            }
+			var message = parameter as InteractionMessage;
 
-            var window = Window.GetWindow(AssociatedObject);
+			if( DirectInteractionMessage != null ) {
+				message = DirectInteractionMessage.Message;
+			}
 
-            if (window == null) return;
+			var window = Window.GetWindow( AssociatedObject );
 
-            if ((!window.IsActive) && InvokeActionOnlyWhenWindowIsActive)
-            {
-                return;
-            }
+			if( window == null ) return;
 
-            if (message != null)
-            {
-                InvokeAction(message);
+			if( ( !window.IsActive ) && InvokeActionOnlyWhenWindowIsActive ) {
+				return;
+			}
 
-                if (DirectInteractionMessage != null)
-                {
-                    DirectInteractionMessage.InvokeCallbacks(message);
-                }
-            }
+			if( message != null ) {
+				InvokeAction( message );
 
-        }
+				if( DirectInteractionMessage != null ) {
+					DirectInteractionMessage.InvokeCallbacks( message );
+				}
+			}
 
-        protected abstract void InvokeAction(InteractionMessage message);
+		}
 
-        /// <summary>
-        /// Viewで直接相互作用メッセージを定義する場合に使用する、DirectInteractionMessageを指定、または取得します。
-        /// </summary>
-        public DirectInteractionMessage DirectInteractionMessage
-        {
-            get { return (DirectInteractionMessage)GetValue(DirectInteractionMessageProperty); }
-            set { SetValue(DirectInteractionMessageProperty, value);}
-        }
+		protected abstract void InvokeAction( InteractionMessage message );
 
-        // Using a DependencyProperty as the backing store for DirectInteractionMessage.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty DirectInteractionMessageProperty =
-            DependencyProperty.Register("DirectInteractionMessage", typeof(DirectInteractionMessage), typeof(InteractionMessageAction<T>), new PropertyMetadata());
+		/// <summary>
+		/// Viewで直接相互作用メッセージを定義する場合に使用する、DirectInteractionMessageを指定、または取得します。
+		/// </summary>
+		public DirectInteractionMessage DirectInteractionMessage {
+			get { return (DirectInteractionMessage)GetValue( DirectInteractionMessageProperty ); }
+			set { SetValue( DirectInteractionMessageProperty, value ); }
+		}
+
+		// Using a DependencyProperty as the backing store for DirectInteractionMessage.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty DirectInteractionMessageProperty =
+			DependencyProperty.Register( "DirectInteractionMessage", typeof( DirectInteractionMessage ), typeof( InteractionMessageAction<T> ), new PropertyMetadata() );
 
 
-        /// <summary>
-        /// Windowがアクティブな時のみアクションを実行するかどうかを指定、または取得します。初期値はtrueです。
-        /// </summary>
-        public bool InvokeActionOnlyWhenWindowIsActive
-        {
-            get { return (bool)GetValue(InvokeActionOnlyWhenWindowIsActiveProperty); }
-            set { SetValue(InvokeActionOnlyWhenWindowIsActiveProperty, value); }
-        }
+		/// <summary>
+		/// Windowがアクティブな時のみアクションを実行するかどうかを指定、または取得します。初期値はfalseです。
+		/// </summary>
+		public bool InvokeActionOnlyWhenWindowIsActive {
+			get { return (bool)GetValue( InvokeActionOnlyWhenWindowIsActiveProperty ); }
+			set { SetValue( InvokeActionOnlyWhenWindowIsActiveProperty, value ); }
+		}
 
-        // Using a DependencyProperty as the backing store for InvokeActionOnlyWhenWindowIsActive.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty InvokeActionOnlyWhenWindowIsActiveProperty =
-            DependencyProperty.Register("InvokeActionOnlyWhenWindowIsActive", typeof(bool), typeof(InteractionMessageAction<T>), new PropertyMetadata(true));
+		// Using a DependencyProperty as the backing store for InvokeActionOnlyWhenWindowIsActive.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty InvokeActionOnlyWhenWindowIsActiveProperty =
+			DependencyProperty.Register( "InvokeActionOnlyWhenWindowIsActive", typeof( bool ), typeof( InteractionMessageAction<T> ), new PropertyMetadata( false ) );
 
-        
-    }
+
+	}
 }

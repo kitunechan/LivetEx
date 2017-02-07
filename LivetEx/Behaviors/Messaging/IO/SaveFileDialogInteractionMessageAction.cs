b0a15fc8e6a1af3c9e1ae2b2e001291a92414e10
kitunechan <22765277+kitunechan@users.noreply.github.com>
2017-02-07 16:58:37 +0900

    DefaultExt の設定を追加

--- LivetEx/Behaviors/Messaging/IO/SaveFileDialogInteractionMessageAction.cs
+++ LivetEx/Behaviors/Messaging/IO/SaveFileDialogInteractionMessageAction.cs
@@ -4,32 +4,28 @@ using System.Windows;
 
 using LivetEx.Messaging;
 
-namespace LivetEx.Behaviors.Messaging.IO
-{
-    /// <summary>
-    /// 「ファイルを保存する」ダイアログを表示するアクションです。<see cref="SavingFileSelectionMessage"/>に対応します。
-    /// </summary>
-    public class SaveFileDialogInteractionMessageAction : InteractionMessageAction<DependencyObject>
-    {
-        protected override void InvokeAction(InteractionMessage message)
-        {
-            var saveFileMessage = message as SavingFileSelectionMessage;
+namespace LivetEx.Behaviors.Messaging.IO {
+	/// <summary>
+	/// 「ファイルを保存する」ダイアログを表示するアクションです。<see cref="SavingFileSelectionMessage"/>に対応します。
+	/// </summary>
+	public class SaveFileDialogInteractionMessageAction: InteractionMessageAction<DependencyObject> {
+		protected override void InvokeAction( InteractionMessage message ) {
+			var saveFileMessage = message as SavingFileSelectionMessage;
 
-            if (saveFileMessage != null)
-            {
-                var dialog = new SaveFileDialog
-                                            {
-                                                FileName = saveFileMessage.FileName,
-                                                InitialDirectory = saveFileMessage.InitialDirectory,
-                                                AddExtension = saveFileMessage.AddExtension,
-                                                CreatePrompt = saveFileMessage.CreatePrompt,
-                                                Filter = saveFileMessage.Filter,
-                                                OverwritePrompt = saveFileMessage.OverwritePrompt,
-                                                Title = saveFileMessage.Title
-                                            };
+			if( saveFileMessage != null ) {
+				var dialog = new SaveFileDialog {
+					FileName = saveFileMessage.FileName,
+					InitialDirectory = saveFileMessage.InitialDirectory,
+					AddExtension = saveFileMessage.AddExtension,
+					CreatePrompt = saveFileMessage.CreatePrompt,
+					Filter = saveFileMessage.Filter,
+					OverwritePrompt = saveFileMessage.OverwritePrompt,
+					Title = saveFileMessage.Title,
+					DefaultExt = saveFileMessage.DefaultExt,
+				};
 
-                saveFileMessage.Response = dialog.ShowDialog().Value ? dialog.FileNames : null;
-            }
-        }
-    }
+				saveFileMessage.Response = dialog.ShowDialog().Value ? dialog.FileNames : null;
+			}
+		}
+	}
 }

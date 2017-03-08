1a8ede7b813a58095c6b507ea408c08719882faf
kitunechan <22765277+kitunechan@users.noreply.github.com>
2017-03-08 20:31:04 +0900

    MessageBox一本化

---
+++ LivetEx/Behaviors/Messaging/MessageBoxInteractionMessageAction.cs
@@ -0,0 +1,32 @@
+﻿using LivetEx.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LivetEx.Behaviors.Messaging {
+	/// <summary>
+	/// メッセージボックスを表示するアクションです。<see cref="MessageBoxMessage"/>に対応します。
+	/// </summary>
+	public class MessageBoxInteractionMessageAction: InteractionMessageAction<FrameworkElement> {
+		protected override void InvokeAction( InteractionMessage message ) {
+			var messageBoxMessage = message as MessageBoxMessage;
+
+			var window = Window.GetWindow( this.AssociatedObject );
+
+			if( messageBoxMessage != null ) {
+				messageBoxMessage.Response = MessageBox.Show(
+					window,
+					messageBoxMessage.Text,
+					messageBoxMessage.Caption,
+					messageBoxMessage.Button,
+					messageBoxMessage.Image,
+					messageBoxMessage.DefaultResult
+					);
+			}
+
+		}
+	}
+}
---
+++ LivetEx/Messaging/MessageBoxMessage.cs
@@ -0,0 +1,147 @@
+﻿using System.Windows;
+
+namespace LivetEx.Messaging {
+	/// <summary>
+	/// 確認相互作用メッセージを表します。
+	/// </summary>
+	public class MessageBoxMessage: ResponsiveInteractionMessage<MessageBoxResult> {
+
+		public MessageBoxMessage() { }
+
+		/// <summary>
+		/// メッセージキーを指定して、新しい相互作用メッセージのインスタンスを生成します。
+		/// </summary>
+		/// <param name="messageKey">メッセージキー</param>
+		public MessageBoxMessage( string messageKey ) : base( messageKey ) { }
+
+		///// <summary>
+		///// 表示するメッセージ・キャプション・メッセージボックスイメージ・メッセージボックスボタン・メッセージキーを指定して、新しい相互作用メッセージのインスタンスを生成します。
+		///// </summary>
+		///// <param name="text">表示するメッセージ</param>
+		///// <param name="caption">キャプション</param>
+		///// <param name="image">メッセージボックスイメージ</param>
+		///// <param name="button">メッセージボックスボタン</param>
+		///// <param name="defaultResult">既定の結果</param>
+		///// <param name="messageKey">メッセージキー</param>
+		//public MessageBoxMessage( string text, string caption, MessageBoxImage image, MessageBoxButton button, MessageBoxResult defaultResult, string messageKey )
+		//	: base( messageKey ) {
+		//	Text = text;
+		//	Caption = caption;
+		//	Image = image;
+		//	Button = button;
+		//	DefaultResult = defaultResult;
+		//}
+
+		///// <summary>
+		///// 表示するメッセージ・キャプション・メッセージボックスイメージ・メッセージボックスボタン・メッセージキーを指定して、新しい相互作用メッセージのインスタンスを生成します。
+		///// </summary>
+		///// <param name="text">表示するメッセージ</param>
+		///// <param name="caption">キャプション</param>
+		///// <param name="image">メッセージボックスイメージ</param>
+		///// <param name="button">メッセージボックスボタン</param>
+		///// <param name="messageKey">メッセージキー</param>
+		//public MessageBoxMessage( string text, string caption, MessageBoxImage image, MessageBoxButton button, string messageKey )
+		//	: this( text, caption, image, button, MessageBoxResult.OK, messageKey ) { }
+
+		///// <summary>
+		///// 表示するメッセージ・キャプション・メッセージボックスイメージ・メッセージキーを指定して、新しい相互作用メッセージのインスタンスを生成します。
+		///// </summary>
+		///// <param name="text">表示するメッセージ</param>
+		///// <param name="caption">キャプション</param>
+		///// <param name="image">メッセージボックスイメージ</param>
+		///// <param name="messageKey">メッセージキー</param>
+		//public MessageBoxMessage( string text, string caption, MessageBoxImage image, string messageKey )
+		//	: this( text, caption, image, MessageBoxButton.OK, messageKey ) { }
+
+		///// <summary>
+		///// 表示するメッセージ・キャプション・メッセージキーを指定して、新しい相互作用メッセージのインスタンスを生成します。
+		///// </summary>
+		///// <param name="text">表示するメッセージ</param>
+		///// <param name="caption">キャプション</param>
+		///// <param name="messageKey">メッセージキー</param>
+		//public MessageBoxMessage( string text, string caption, string messageKey )
+		//	: this( text, caption, MessageBoxImage.None, messageKey ) { }
+
+		///// <summary>
+		///// 表示するメッセージ・キャプションを指定して、新しい相互作用メッセージのインスタンスを生成します。
+		///// </summary>
+		///// <param name="text">表示するメッセージ</param>
+		///// <param name="caption">キャプション</param>
+		//public MessageBoxMessage( string text, string caption )
+		//	: this( text, caption, null ) { }
+
+
+		/// <summary>
+		/// 派生クラスでは必ずオーバーライドしてください。Freezableオブジェクトとして必要な実装です。<br/>
+		/// 通常このメソッドは、自身の新しいインスタンスを返すように実装します。
+		/// </summary>
+		/// <returns>自身の新しいインスタンス</returns>
+		protected override Freezable CreateInstanceCore() {
+			return new MessageBoxMessage();
+		}
+
+		/// <summary>
+		/// 表示するメッセージを指定、または取得します。
+		/// </summary>
+		public string Text {
+			get { return (string)GetValue( TextProperty ); }
+			set { SetValue( TextProperty, value ); }
+		}
+
+		// Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty TextProperty =
+			DependencyProperty.Register( "Text", typeof( string ), typeof( MessageBoxMessage ), new PropertyMetadata( null ) );
+
+
+		/// <summary>
+		/// キャプションを指定、または取得します。
+		/// </summary>
+		public string Caption {
+			get { return (string)GetValue( CaptionProperty ); }
+			set { SetValue( CaptionProperty, value ); }
+		}
+
+		// Using a DependencyProperty as the backing store for Caption.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty CaptionProperty =
+			DependencyProperty.Register( "Caption", typeof( string ), typeof( MessageBoxMessage ), new PropertyMetadata( null ) );
+
+
+
+		/// <summary>
+		/// メッセージボックスイメージを指定、または取得します。
+		/// </summary>
+		public MessageBoxImage Image {
+			get { return (MessageBoxImage)GetValue( ImageProperty ); }
+			set { SetValue( ImageProperty, value ); }
+		}
+
+		// Using a DependencyProperty as the backing store for Image.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty ImageProperty =
+			DependencyProperty.Register( "Image", typeof( MessageBoxImage ), typeof( MessageBoxMessage ), new PropertyMetadata() );
+
+
+		/// <summary>
+		/// メッセージボックスボタンを指定、または取得します。
+		/// </summary>
+		public MessageBoxButton Button {
+			get { return (MessageBoxButton)GetValue( ButtonProperty ); }
+			set { SetValue( ButtonProperty, value ); }
+		}
+
+		// Using a DependencyProperty as the backing store for Button.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty ButtonProperty =
+			DependencyProperty.Register( "Button", typeof( MessageBoxButton ), typeof( MessageBoxMessage ), new PropertyMetadata( MessageBoxButton.OK ) );
+
+		/// <summary>
+		/// メッセージボックスの既定の結果を指定、または取得します。
+		/// </summary>
+		public MessageBoxResult DefaultResult {
+			get { return (MessageBoxResult)GetValue( DefaultResultProperty ); }
+			set { SetValue( DefaultResultProperty, value ); }
+		}
+
+		// Using a DependencyProperty as the backing store for DefaultResult.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty DefaultResultProperty =
+			DependencyProperty.Register( "DefaultResult", typeof( MessageBoxResult ), typeof( MessageBoxMessage ), new PropertyMetadata( MessageBoxResult.OK ) );
+	}
+}

a73e1c74a10a144be618f2f0319f510a67c8e3f4
kitunechan <22765277+kitunechan@users.noreply.github.com>
2017-09-01 09:38:32 +0900

    Xamlでint,double,float,boolを簡単に書けるMarkupExtensionの追加

---
+++ LivetEx/SystemTypeExtension.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace LivetEx {
+	public class SystemTypeExtension : MarkupExtension {
+		private object parameter;
+
+		public int Int { set { parameter = value; } }
+		public double Double { set { parameter = value; } }
+		public float Float { set { parameter = value; } }
+		public bool Bool { set { parameter = value; } }
+		
+
+		public override object ProvideValue( IServiceProvider serviceProvider ) {
+			return parameter;
+		}
+	}
+
+	public class InExtensiont : MarkupExtension {
+		public InExtensiont( int value ) {
+			this.value = value;
+		}
+
+		public int value { get; set; }
+
+		public override object ProvideValue( IServiceProvider serviceProvider ) {
+			return value;
+		}
+	}
+
+	public class DoubleExtension : MarkupExtension {
+		public DoubleExtension( double value ) {
+			this.value = value;
+		}
+
+		public double value { get; set; }
+
+		public override object ProvideValue( IServiceProvider serviceProvider ) {
+			return value;
+		}
+	}
+
+	public class FloatExtension : MarkupExtension {
+		public FloatExtension( float value ) {
+			this.value = value;
+		}
+
+		public float value { get; set; }
+
+		public override object ProvideValue( IServiceProvider serviceProvider ) {
+			return value;
+		}
+	}
+
+	public class BoolExtension : MarkupExtension {
+		public BoolExtension( bool value ) {
+			this.value = value;
+		}
+
+		public bool value { get; set; }
+
+		public override object ProvideValue( IServiceProvider serviceProvider ) {
+			return value;
+		}
+	}
+
+
+}
